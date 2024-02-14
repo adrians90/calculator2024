6000e3488e1b9491775f8c0202a0c8ffc5f14e41
Adrian <adriansandu9090@gmail.com>
2024-02-14 19:12:14 +0000

    display clears now

--- script.js
+++ script.js
@@ -56,10 +56,34 @@ class Calculator {
         this.previousOperand = ""
 
     }
-
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split(".")[0])
+        const decimalDigits = stringNumber.split(".")[1]
+        let integerDisplay
+        if(isNaN(integerDigits)) {
+            integerDisplay = ""
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = 
+            this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null) {
+            this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ""
+        }
+         
     }
 }
 
@@ -67,7 +91,7 @@ const numberButtons = document.querySelectorAll("[data-number]")
 const operationButtons = document.querySelectorAll("[data-operation]")
 const equalsButton = document.querySelector("[data-equals]")
 const deleteButton = document.querySelector("[data-delete]")
-const allClearButton = document.querySelector("[data-all-cleaer]")
+const allClearButton = document.querySelector("[data-all-clear]")
 const previousOperandTextElement = document.querySelector("[data-previous-operand]")
 const currentOperandTextElement = document.querySelector("[data-current-operand]")
 const calculator = new Calculator(previousOperandTextElement,
@@ -97,3 +121,7 @@ allClearButton.addEventListener("click", button => {
     calculator.updateDisplay()
 })
 
+deleteButton.addEventListener("click", button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
